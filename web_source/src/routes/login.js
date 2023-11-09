de722c4a38e807553ff7114f780fef559f81ddda
Nguyễn Thái Tân <nguyenthaitan02@gmail.com>
2023-11-09 20:35:02 +0700

    2fa

--- web_source/src/routes/login.js
+++ web_source/src/routes/login.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const loginController = require('../app/controllers/LoginController');
+const user2faController = require('../app/controllers/User2FAController');
 
 router.get('/', loginController.index);
+router.get('/2fa', user2faController.index);
 
 module.exports = router;
