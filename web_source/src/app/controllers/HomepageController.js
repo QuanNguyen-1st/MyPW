e574e8ac78c1bb684280878d0dd91b5e8e0118c9
Cyese <87186561+Cyese@users.noreply.github.com>
2023-11-25 23:23:48 +0700

    Fixed homepage

--- web_source/src/app/controllers/HomepageController.js
+++ web_source/src/app/controllers/HomepageController.js
@@ -1,9 +1,14 @@
 class HomepageController {
     // [GET] /
     index(req, res, next) {
+        // res.render('test', {
+        //     homepageActive: true
+        // });
         res.render('homepage', {
             homepageActive: true
         });
+    
+    
     }
 }
 
