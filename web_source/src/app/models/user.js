7e2afd34950cd0bc917eae49936d641116778759
Nguyễn Thái Tân <nguyenthaitan02@gmail.com>
2023-12-22 02:34:34 +0700

    user list filter

--- web_source/src/app/models/user.js
+++ web_source/src/app/models/user.js
@@ -35,7 +35,7 @@ class UserModel {
                 const year = date.getFullYear();
                 const month = String(date.getMonth() + 1).padStart(2, '0');
                 const day = String(date.getDate()).padStart(2, '0');
-                return `${day}-${month}-${year}`;
+                return `${day}/${month}/${year}`;
             }
             return formattedResult;
         } catch (err) {
