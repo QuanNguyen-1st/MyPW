65ca4a8049a2577f62b95e584ca4478ea2823c68
Nguyễn Thái Tân <nguyenthaitan02@gmail.com>
2023-12-22 03:18:21 +0700

    admin feedback filter

--- web_source/src/app/models/feedback.js
+++ web_source/src/app/models/feedback.js
@@ -6,7 +6,19 @@ class AccountModel {
         try {
             const pool = await poolPromise;
             const result = await pool.request().query('SELECT * FROM FEEDBACK');
-            return result.recordset;
+            const formattedResult = result.recordset.map((row) => {
+                return {
+                    ...row,
+                    time: row.time ? formatDate(row.time) : null
+                };
+            }); 
+            function formatDate(date) {
+                const year = date.getFullYear();
+                const month = String(date.getMonth() + 1).padStart(2, '0');
+                const day = String(date.getDate()).padStart(2, '0');
+                return `${day}/${month}/${year}`;
+            }
+            return formattedResult;
         } catch (err) {
             console.error('Error executing query:', err);
             throw err;
